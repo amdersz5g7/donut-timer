fb768c901481c43babda72add20c2958ff0c961d
amdersz5g7 <amdersz5g7@gmail.com>
2026-07-03 21:57:23 +0700

    feat: display note with notebook icon on timer card

--- src/App.svelte
+++ src/App.svelte
@@ -811,6 +811,15 @@
                   <StopCircleIcon size="20" />
                   <span class="justinfo">{timer.finish_at}</span>
                 </div>
+                {#if timer.note}
+                  <div>
+                    <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" style="vertical-align: middle; color: #888;">
+                      <path d="M4 19.5A2.5 2.5 0 0 1 6.5 17H20"></path>
+                      <path d="M6.5 2H20v20H6.5A2.5 2.5 0 0 1 4 19.5v-15A2.5 2.5 0 0 1 6.5 2z"></path>
+                    </svg>
+                    <span class="justinfo" style="color: #888; font-style: italic;">{timer.note}</span>
+                  </div>
+                {/if}
                 {#if !timer.done}
                   <button
                     class="xprimary secondary edit-timer-btn"
